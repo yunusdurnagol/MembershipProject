a15b074819bf5ac825f7bcaa21c44ffff41d5376
yunus <yunusdurnagol@gmail.com>
2017-07-08 17:07:02 +0300

    Begin

--- AuthenticationAndAuthorization/Controllers/HomeController.cs
+++ AuthenticationAndAuthorization/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace AuthenticationAndAuthorization.Controllers
 {
+    [AllowAnonymouss]
     public class HomeController : Controller
     {
         public ActionResult Index()
